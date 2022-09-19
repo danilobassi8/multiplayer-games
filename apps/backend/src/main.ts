6cf9da94a48ed227aab75d02b777d5e7fa3f4991
Bassi, Danilo <dbassi@irobot.com>
2022-09-19 19:58:12 -0300

    eslint config changes + colyseus integration

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,22 +1,26 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
+import { MinesweeperRoom } from './GameRooms/Minesweeper.room';
 import { NestFactory } from '@nestjs/core';
-
+import { Server } from 'colyseus';
 import { AppModule } from './app/app.module';
+
+const GAME_ROOMS = [MinesweeperRoom];
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
+
+  const gameServer = new Server();
+
+  GAME_ROOMS.forEach((room) => {
+    Logger.log(`-- Registering room: ${room.name}`);
+    gameServer.define(room.name, room);
+  });
+
+  // attach Colyseus into the existing http server from NestJS
+  gameServer.attach({ server: app.getHttpServer() });
   await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
+  Logger.log(`🚀 Application is running on: http://localhost:${port}`);
 }
 
 bootstrap();
