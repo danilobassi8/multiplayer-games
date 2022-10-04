b5dc269be0115ee61d34c43b64369224960cca93
Danilo <danilo@pop-os.localdomain>
2022-10-04 19:47:31 -0300

    add isLocked boolean}

--- apps/backend/src/GameRooms/Minesweeper.room.ts
+++ apps/backend/src/GameRooms/Minesweeper.room.ts
@@ -7,7 +7,7 @@ export class MinesweeperRoom extends Room {
 
   // When room is initialized
   onCreate(options: any) {
-    this.setMetadata({ creator: options.player });
+    this.setMetadata({ creator: options.player, isPrivate: !!options.password });
     Logger.log('Room created', { options });
   }
 
