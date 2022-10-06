c9968e6e8b311fe01a2163eeb53da7c2dcb91551
Bassi, Danilo <dbassi@irobot.com>
2022-10-06 19:40:03 -0300

    add height as an input on matrix param

--- apps/frontend/src/app/minesweeper/game-matrix/game-matrix.component.ts
+++ apps/frontend/src/app/minesweeper/game-matrix/game-matrix.component.ts
@@ -1,6 +1,6 @@
 import { MinesweeperService } from './../../services/minesweeper.service';
 import { GameConfig, Matrix, Cell } from './../../../../../../libs/minesweeper.lib';
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'minesweeper-game-matrix',
@@ -10,12 +10,21 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 export class GameMatrixComponent implements OnInit {
   @Input() matrix: Matrix;
   @Input() currentConfig: GameConfig;
+  @Input() maxHeightInPixels: number = 750;
   @Output() cellClicked = new EventEmitter<Cell>();
   @Output() cellRightClicked = new EventEmitter<Cell>();
+
+  public gridTemplateFrString: string = '';
 
   constructor(private game: MinesweeperService) {
     this.currentConfig = game.gameConfig.easy;
     this.matrix = new Matrix(this.currentConfig);
+  }
+
+  ngOnChanges(change: SimpleChanges) {
+    if (change['currentConfig']) {
+      this.gridTemplateFrString = `repeat(${this.currentConfig.size},calc(min(${this.maxHeightInPixels}px, 90vw) / ${this.currentConfig.size}))`;
+    }
   }
 
   ngOnInit(): void {}
